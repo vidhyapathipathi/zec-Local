d03d8cbc367dc4e57400e376a3ef53c655d43057
vidhyapathipathi <vkandhasamy@aezion.com>
2018-08-16 10:41:54 +0530

    Initial Commit

---
+++ ECI-Local/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ECI-Local
+//
+//  Created by Vidhyapathi Kandhasamy on 8/16/18.
+//  Copyright © 2018 aezion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
